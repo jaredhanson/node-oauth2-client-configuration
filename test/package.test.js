99fd4fac1aa705f97f016f9ed90b9c2833424bdc
Jared Hanson <jaredhanson@gmail.com>
2016-03-02 15:24:26 -0800

    Parse additional metadata.

--- test/package.test.js
+++ test/package.test.js
@@ -6,8 +6,8 @@ var expect = require('chai').expect;
 
 describe('oauth2-client-configuration', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  it('should export function', function() {
+    expect(pkg).to.be.a('function');
   });
   
 });
